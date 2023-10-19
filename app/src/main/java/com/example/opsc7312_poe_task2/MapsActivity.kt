51fdeb4ba53ab3b7b0eda58f3fa2f8df0fc59bd6
UltraBound24 <93461637+UltraBound24@users.noreply.github.com>
2023-10-19 15:14:46 +0200

    commit aiden

--- app/src/main/java/com/example/opsc7312_poe_task2/MapsActivity.kt
+++ app/src/main/java/com/example/opsc7312_poe_task2/MapsActivity.kt
@@ -1,15 +1,25 @@
 package com.example.opsc7312_poe_task2
 
-
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import androidx.core.app.ActivityCompat
 import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.opsc7312_poe_task2.databinding.ActivityMapsBinding
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.maps.*
 
 
@@ -17,9 +27,10 @@ import com.google.android.gms.maps.*
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mapView: MapView
-    private lateinit var googleMap: GoogleMap
-    private lateinit var binding: ActivityMapsBinding
-    private val LOCATION_PERMISSION_REQUEST_CODE = 404
+    private lateinit var mMap: GoogleMap
+    //private lateinit var binding: ActivityMapsBinding
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,46 +38,46 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
 
 
-        mapView = findViewById(R.id.mapView)
-        mapView.onCreate(savedInstanceState)
+       // mapView = findViewById(R.id.mapView)
+        //mapView.onCreate(savedInstanceState)
 
-        mapView.getMapAsync { map ->
-            googleMap = map
+       // mapView.getMapAsync { map ->
+        ///    mMap = map
 
-            binding = ActivityMapsBinding.inflate(layoutInflater)
-            setContentView(binding.root)
+          //  binding = ActivityMapsBinding.inflate(layoutInflater)
+          //  setContentView(binding.root)
 
-            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-            val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-            mapFragment.getMapAsync(this)
-        }
-
+       // }
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
     }
 
+
+
     override fun onResume() {
         super.onResume()
-        mapView.onResume()
+       // mapView.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-        mapView.onPause()
+        //mapView.onPause()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mapView.onDestroy()
+        //mapView.onDestroy()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
-        mapView.onLowMemory()
+       // mapView.onLowMemory()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        mapView.onSaveInstanceState(outState)
+       // mapView.onSaveInstanceState(outState)
     }
 
     /**
@@ -80,27 +91,72 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
 
 
-
     override fun onMapReady(googleMap: GoogleMap) {
-        val mMap = googleMap
-
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
 
         // Enable the My Location layer on the map
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            mMap.isMyLocationEnabled = true
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            enableMyLocation()
         } else {
             // Request the missing location permission
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+        private fun enableMyLocation() {
+            mMap.isMyLocationEnabled = true
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mMap.isMyLocationEnabled = true
+
+                val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+                fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                    if (location != null) {
+                        val userLatLng = LatLng(location.latitude, location.longitude)
+                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 12f))
+
+                        // Fetch eBird hotspots based on user's location
+                        fetchBirdingHotspots(location.latitude, location.longitude)
+
+                        // Add a marker for the user's location
+                        mMap.addMarker(MarkerOptions().position(userLatLng).title("Your Location"))
+                    }
+                }
+            } else {
+                // Permission is not granted, request it
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    LOCATION_PERMISSION_REQUEST_CODE
+                )
+            }
         }
 
-        // Add a marker in the user's current location (if you want)
-        mMap.addMarker(MarkerOptions().position(LatLng(0.0, 0.0)).title("Your Location"))
+    fun fetchBirdingHotspots(latitude: Double, longitude: Double) {
+        val eBirdAPIUrl = "https://api.ebird.org/v2/ref/geo/loc/recent"
 
-        // Move the camera to the user's current location
-        mMap.setOnMyLocationButtonClickListener {
-            // Handle location button click if needed
-            true
-        }
+        val requestQueue = Volley.newRequestQueue(this)
 
+        val request = JsonObjectRequest(
+            Request.Method.GET,
+            "$eBirdAPIUrl?lat=$latitude&lng=$longitude",
+            null,
+            { response ->
+                // Handle the API response
+            },
+            { error ->
+                // Handle errors
+            }
+        )
+
+        requestQueue.add(request)
     }
 }
