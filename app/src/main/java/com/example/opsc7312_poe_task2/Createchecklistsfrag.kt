50bc59dd6aad82fd77f50539cc40832803cce513
Kylenewmark1 <104084727+Kylenewmark1@users.noreply.github.com>
2023-10-19 14:04:07 +0200

    update

--- app/src/main/java/com/example/opsc7312_poe_task2/Createchecklistsfrag.kt
+++ app/src/main/java/com/example/opsc7312_poe_task2/Createchecklistsfrag.kt
@@ -1,20 +1,36 @@
 package com.example.opsc7312_poe_task2
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.opsc7312_poe_task2.databinding.FragmentCreatechecklistsfragBinding
 
 class Createchecklistsfrag : Fragment() {
+    private var binding: FragmentCreatechecklistsfragBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout XML resource file 'fragment_createchecklistsfrag' to create the user interface of this fragment.
-        val createchecklistsfrag = inflater.inflate(R.layout.fragment_createchecklistsfrag, container, false)
+        binding = FragmentCreatechecklistsfragBinding.inflate(inflater, container, false)
+        val createchecklistsfrag = binding?.root
 
-        // Return the View representing the user interface of this fragment to be displayed on the screen.
+        // Set a click listener for your button
+        binding?.button4?.setOnClickListener {
+            val name = binding?.editTextText2?.text.toString()
+            val date = binding?.editTextText4?.text.toString()
+            val location = binding?.editTextText3?.text.toString()
+
+            // Handle your button click here, you can use the name, date, and location as needed.
+        }
+
         return createchecklistsfrag
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 }
