cd185ff5368c5135b3af654d8042836aa30ecc11
JordanTitus <jordantitus2301@gmail.com>
2023-10-19 18:42:23 +0200

    commit chat changes

--- app/src/main/java/com/example/opsc7312_poe_task2/BirdListAdapter.kt
+++ app/src/main/java/com/example/opsc7312_poe_task2/BirdListAdapter.kt
@@ -6,11 +6,13 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.fragment.app.Fragment
 
 data class BirdItem(val img: Int, val name: String, val dateTime: String, val location: String)
 
-class BirdListAdapter(private val itemList: List<BirdItem>) :
+class BirdListAdapter(private val itemList: List<BirdItem>, private val fragment: Fragment) :
     RecyclerView.Adapter<BirdListAdapter.ViewHolder>() {
+
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val ivImage: ImageView = itemView.findViewById(R.id.ivImage)
         val tvName: TextView = itemView.findViewById(R.id.txtName)
@@ -20,7 +22,7 @@ class BirdListAdapter(private val itemList: List<BirdItem>) :
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.birds_list, parent, false)
+            .inflate(R.layout.fragment_birdsfrag, parent, false)
         return ViewHolder(view)
     }
 
