51842de54bbd19131a16c3550071b191d4e52f7d
Kylenewmark1 <104084727+Kylenewmark1@users.noreply.github.com>
2023-11-14 12:21:39 +0200

    update

--- app/src/main/java/com/example/opsc7312_poe_task2/HomeFragment.kt
+++ app/src/main/java/com/example/opsc7312_poe_task2/HomeFragment.kt
@@ -8,11 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -64,14 +60,7 @@ class HomeFragment : Fragment() {
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
+
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
